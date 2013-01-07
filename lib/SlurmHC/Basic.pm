5678ca4123b7258cd1ed991b5ec118084cd30cfa
Matej Batič <matej.batic@ijs.si>
2013-01-07 09:32:51 +0100

    About to see 3J.

--- lib/SlurmHC/Basic.pm
+++ lib/SlurmHC/Basic.pm
@@ -64,7 +64,6 @@ sub load_average {
     #and
     # SlurmHC::Basic::load_average( ... )
     #and not run into "Odd number of elements" or something similar?
-    #(talk to JJJ)
 
     #undefined defaults at first
     my %defaults = ( load_max_1min => undef,
@@ -75,6 +74,8 @@ sub load_average {
 
     #in case of no argument
     #perform 15min average test with load_max = 1.5*n_cpu
+    #
+    #this is NOT ok: what about if all are <0?
     if(not defined $arg{load_max_1min} && 
        not defined $arg{load_max_5min} &&
        not defined $arg{load_max_15min}) {
@@ -107,17 +108,20 @@ sub load_average {
     if(defined $arg{load_max_1min}){
       if ( $load_1 > $arg{load_max_1min}*$ncpu ) {
 	# load is above set limit
-	$error=1;
+  print "1min above limit: $load_1 > $arg{load_max_1min}*$ncpu\n";
+  $error=1;
       }
     }
     if(defined $arg{load_max_5min}){
       if ( $load_5 > $arg{load_max_5min}*$ncpu ) {
 	# load is above set limit
+  print "5min above limit: $load_5 > $arg{load_max_5min}*$ncpu\n";
 	$error+=2;
       }
     }
     if(defined $arg{load_max_15min}){
       if ( $load_15 > $arg{load_max_15min}*$ncpu ) {
+  print "15min above limit: $load_15 > $arg{load_max_15min}*$ncpu\n";
 	# load is above set limit
 	# 	push $self->SUPER::{error},"(E) Load: <15min> to high: $load_15, should be below ".($arg{load_max_15min}*$ncpu);
 	$error+=3;
@@ -136,6 +140,7 @@ sub load_average {
       }
       else{
 	#warning??
+  return 1;
       }
     }
     else{
