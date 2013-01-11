641640568010762b6047b064f534208d9cf4f223
Matej Batič <matej.batic@ijs.si>
2013-01-11 15:14:26 +0100

    Elapsed time + both :: or -> calls working now also in Load.

--- lib/SlurmHC/Load.pm
+++ lib/SlurmHC/Load.pm
@@ -4,124 +4,140 @@ use strict;
 use warnings;
 
 use parent 'SlurmHC';
+
+use Time::HiRes qw(gettimeofday tv_interval);
 use vars qw($VERSION);
 
 sub n_cpu {
-    my @ncpu = grep(/^processor/,`cat /proc/cpuinfo`);
-    return $#ncpu+1;
+  my @ncpu = grep(/^processor/,`cat /proc/cpuinfo`);
+  return $#ncpu+1;
 }
 
 
 sub run{
-    my $self;
-    $self = shift if ref $_[0] and $_[0]->can('isa') and  $_[0]->isa('SlurmHC::Load');
-    my $arg = { @_ };
+  #start timing
+  my $start_time=[gettimeofday]; #start timing
 
-    #results hashref
-    my $results={
-		 info      => [],
-		 warning   => [],
-		 error     => [],
-		 result    => 0
-		};
+  my $self;
+  if($_[0] || ref $_[0]){
+    $self = shift if $_[0]->can('isa') and  $_[0]->isa('SlurmHC::Load');
+  }
 
-    my $available_limits= { load_max_1min=>'', load_max_5min=>'', load_max_15min=>'' };
+  my $arg = { @_ };
 
-    if(keys $arg){
-      foreach(keys $arg){
-	if(not defined $available_limits->{$_}){
-	  push $results->{warning}, (caller(0))[3]." Unavailable option \"$_\"!";
-	}
+  #results hashref
+  my $results={
+	       info      => [],
+	       warning   => [],
+	       error     => [],
+	       result    => 0,
+	       elapsed   => 0,
+	      };
+
+  my $available_limits= { load_max_1min=>'', load_max_5min=>'', load_max_15min=>'' };
+
+  if (keys $arg) {
+    foreach (keys $arg) {
+      if (not defined $available_limits->{$_}) {
+	push $results->{warning}, (caller(0))[3]." Unavailable option \"$_\"!";
       }
     }
+  }
 
-    #check if arguments are positive
-    #undefine if not, keep only 15min average, defaulting it to 1.5
-    if(defined $arg->{load_max_1min} and $arg->{load_max_1min}<0.){
-      $arg->{load_max_1min}=undef;
-      push $results->{warning}, (caller(0))[3]." Negative limit for 1min average.";
-    }
-    if(defined $arg->{load_max_5min} and $arg->{load_max_5min}<0.){
-      $arg->{load_max_5min}=undef;
-      push $results->{warning}, (caller(0))[3]." Negative limit for 5min average.";
-    }
-    if(defined $arg->{load_max_15min} and $arg->{load_max_15min}<0.0){
-      $arg->{load_max_15min}=undef;
-      push $results->{warning}, (caller(0))[3]." Negative limit for 15min average.";
-    }
-    if(not defined $arg->{load_max_1min}
-       and not defined $arg->{load_max_5min}
-       and not defined $arg->{load_max_15min}){
-      $arg->{load_max_15min}=1.5;
-      push $results->{warning},
-	(caller(0))[3]." Using default test: 15min average with load_max_15min=$arg->{load_max_15min}.";
-    }
+  #check if arguments are positive
+  #undefine if not, keep only 15min average, defaulting it to 1.5
+  if (defined $arg->{load_max_1min} and $arg->{load_max_1min}<0.) {
+    $arg->{load_max_1min}=undef;
+    push $results->{warning}, (caller(0))[3]." Negative limit for 1min average.";
+  }
+  if (defined $arg->{load_max_5min} and $arg->{load_max_5min}<0.) {
+    $arg->{load_max_5min}=undef;
+    push $results->{warning}, (caller(0))[3]." Negative limit for 5min average.";
+  }
+  if (defined $arg->{load_max_15min} and $arg->{load_max_15min}<0.0) {
+    $arg->{load_max_15min}=undef;
+    push $results->{warning}, (caller(0))[3]." Negative limit for 15min average.";
+  }
+  if (not defined $arg->{load_max_1min}
+      and not defined $arg->{load_max_5min}
+      and not defined $arg->{load_max_15min}) {
+    $arg->{load_max_15min}=1.5;
+    push $results->{warning},
+      (caller(0))[3]." Using default test: 15min average with load_max_15min=$arg->{load_max_15min}.";
+  }
 
 
-    my $ncpu=n_cpu();
+  my $ncpu=n_cpu();
 
-    open(LOAD, "/proc/loadavg") or do {
-      push $results->{error}, (caller(0))[3]."Unable to get server load: $!";
-      $results->{result}=1;
-      return $results;
-    };
+  open(LOAD, "/proc/loadavg") or do {
+    push $results->{error}, (caller(0))[3]."Unable to get server load: $!";
+    $results->{result}=1;
 
-    my $load_avg = <LOAD>;
-    close LOAD;
-    chop $load_avg;
-    my ( $load_1, $load_5, $load_15 ) = split /\s/, $load_avg;
-
-    if(defined $arg->{load_max_1min}){
-      if ( $load_1 > $arg->{load_max_1min}*$ncpu ) {
-	# load is above set limit
-	push $results->{warning},
-	  (caller(0))[3]." 1min load average above limit: $load_1 > "
-	    .($arg->{load_max_1min}*$ncpu);
-	$results->{result}+=1;
-      }
-    }
-    if(defined $arg->{load_max_5min}){
-      if ( $load_5 > $arg->{load_max_5min}*$ncpu ) {
-	# load is above set limit
-	push $results->{warning},
-	  (caller(0))[3]." 5min load average above limit: $load_5 > "
-	    .($arg->{load_max_5min}*$ncpu);
-	$results->{result}+=2;
-      }
-    }
-    if(defined $arg->{load_max_15min}){
-      if ( $load_15 > $arg->{load_max_15min}*$ncpu ) {
-	# load is above set limit
-	push $results->{warning},
-	  (caller(0))[3]." 15min load average above limit: $load_15 > "
-	    .($arg->{load_max_15min}*$ncpu);
-	$results->{result}+=3;
-      }
-    }
-    my $error=$results->{result};
-
-    my $total_error_check=0;
-    $total_error_check+=1 if defined $arg->{load_max_1min};
-    $total_error_check+=2 if defined $arg->{load_max_5min};
-    $total_error_check+=3 if defined $arg->{load_max_15min};
-
-    if($error>0){
-      if($error==$total_error_check){
-	push $results->{error},
-	  (caller(0))[3]." Tested load average is too high.";
-	$results->{result}=1;
-      }
-      else{
-	#warning was already issued, return with 0
-	$results->{result}=0;
-      }
-    }
-    else{
-      push $results->{info}, (caller(0))[3]." Tested load average is ok.";
-      $results->{result}=0;
-    }
+    #stop timing and add elapsed time
+    my $end_time = [gettimeofday];
+    $results->{elapsed} = tv_interval $start_time, $end_time;
 
     return $results;
+  };
+
+  my $load_avg = <LOAD>;
+  close LOAD;
+  chop $load_avg;
+  my ( $load_1, $load_5, $load_15 ) = split /\s/, $load_avg;
+
+  if (defined $arg->{load_max_1min}) {
+    if ( $load_1 > $arg->{load_max_1min}*$ncpu ) {
+      # load is above set limit
+      push $results->{warning},
+	(caller(0))[3]." 1min load average above limit: $load_1 > "
+	  .($arg->{load_max_1min}*$ncpu);
+      $results->{result}+=1;
+    }
+  }
+  if (defined $arg->{load_max_5min}) {
+    if ( $load_5 > $arg->{load_max_5min}*$ncpu ) {
+      # load is above set limit
+      push $results->{warning},
+	(caller(0))[3]." 5min load average above limit: $load_5 > "
+	  .($arg->{load_max_5min}*$ncpu);
+      $results->{result}+=2;
+    }
+  }
+  if (defined $arg->{load_max_15min}) {
+    if ( $load_15 > $arg->{load_max_15min}*$ncpu ) {
+      # load is above set limit
+      push $results->{warning},
+	(caller(0))[3]." 15min load average above limit: $load_15 > "
+	  .($arg->{load_max_15min}*$ncpu);
+      $results->{result}+=3;
+    }
+  }
+  my $error=$results->{result};
+
+  my $total_error_check=0;
+  $total_error_check+=1 if defined $arg->{load_max_1min};
+  $total_error_check+=2 if defined $arg->{load_max_5min};
+  $total_error_check+=3 if defined $arg->{load_max_15min};
+
+  if ($error>0) {
+    if ($error==$total_error_check) {
+      push $results->{error},
+	(caller(0))[3]." Tested load average is too high.";
+      $results->{result}=1;
+    } else {
+      #warning was already issued, return with 0
+      $results->{result}=0;
+    }
+  } else {
+    push $results->{info}, (caller(0))[3]." Tested load average is ok.";
+    $results->{result}=0;
+  }
+
+  #stop timing and add elapsed time
+  my $end_time = [gettimeofday];
+  $results->{elapsed} = tv_interval $start_time, $end_time;
+
+  return $results;
 }
 
 1;
--- t/003_run_Load.t
+++ t/003_run_Load.t
@@ -8,47 +8,53 @@ use Data::Dumper;
 #try to: use SlurmHC
 use_ok( 'SlurmHC::Load' );
 
+sub check_timing{
+    my $time=shift;
+    return ($time>0) ? 1 : 0;
+}
+
 #populate results to (deeply) compare with results from tests
 my $res={
     info      => [],
     warning   => [],
     error     => [],
-    result    => 0
+    result    => 0,
+    elapsed   => code(\&check_timing),
 };
 
 #following tests should not fail
 push $res->{info}, re("Tested load average is ok.");
 
 #try running load_average
-my $a=SlurmHC::Load::run( );
+my $a=SlurmHC::Load->run( );
 #print Dumper($a);
 push $res->{warning}, re("Using default test");
 cmp_deeply $a, $res, "Is load average below 1.5*n_cpu?";
 $res->{warning}=[];
 
 my $load=4;
-$a=SlurmHC::Load::run( load_max_1min=>$load );
+$a=SlurmHC::Load->run( load_max_1min=>$load );
 cmp_deeply $a, $res, "Is 1min load average below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_5min=>$load );
+$a=SlurmHC::Load->run( load_max_5min=>$load );
 cmp_deeply $a, $res, "Is 5min load average below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_15min=>$load );
+$a=SlurmHC::Load->run( load_max_15min=>$load );
 cmp_deeply $a, $res, "Is 15min load average below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_5min=>$load );
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_5min=>$load );
 cmp_deeply $a, $res, "Are 1min and 5min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_15min=>$load );
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_15min=>$load );
 cmp_deeply $a, $res, "Are 1min and 15min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_5min=>$load, load_max_15min=>$load );
+$a=SlurmHC::Load->run( load_max_5min=>$load, load_max_15min=>$load );
 cmp_deeply $a, $res, "Are 5min and 15min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_5min=>$load, load_max_15min=>$load );
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_5min=>$load, load_max_15min=>$load );
 cmp_deeply $a, $res, "Are 1min, 5min and 15min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_4min=>$load, load_max_9min=>$load );
+$a=SlurmHC::Load->run( load_max_4min=>$load, load_max_9min=>$load );
 #print Dumper($a);
 push $res->{warning}, re("Unavailable option");
 push $res->{warning}, re("Unavailable option");
@@ -62,27 +68,27 @@ $res->{result}=1;
 $res->{info}=[];
 push $res->{warning}, re("load average above limit");
 push $res->{error}, re("Tested load average is too high");
-$a=SlurmHC::Load::run( load_max_1min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Is 1min load average below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_5min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_5min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Is 5min load average below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_15min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_15min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Is 15min load average below $load*n_cpu?";
 
 push $res->{warning}, re("load average above limit"); #two tests giving warnings!
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_5min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_5min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Are 1min and 5min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_15min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_15min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Are 1min and 15min load averages below $load*n_cpu?";
 
-$a=SlurmHC::Load::run( load_max_5min=>$load, load_max_15min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_5min=>$load, load_max_15min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Are 5min and 15min load averages below $load*n_cpu?";
 
 push $res->{warning}, re("load average above limit"); #three tests giving warnings!
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_5min=>$load, load_max_15min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_5min=>$load, load_max_15min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Are 1min, 5min and 15min load averages below $load*n_cpu?";
 
 $res->{result}=0;
@@ -90,10 +96,10 @@ $res->{info}=[];
 $res->{warning}=[];
 $res->{error}=[];
 push $res->{warning}, re("load average above limit");
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_5min=>5 ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_5min=>5 ); #print Dumper($a);
 cmp_deeply $a, $res, "Are both 1min and 5min load averages failing? They shouldn't.";
 
-$a=SlurmHC::Load::run( load_max_1min=>$load, load_max_15min=>5 ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load, load_max_15min=>5 ); #print Dumper($a);
 cmp_deeply $a, $res, "Are both 1min and 15min load averages failing? They shouldn't.";
 
 #try running load_average with negative value
@@ -105,13 +111,13 @@ $res->{error}=[];
 push $res->{info}, re("Tested load average is ok.");
 push $res->{warning}, re("Negative limit");
 push $res->{warning}, re("Using default test");
-$a=SlurmHC::Load::run( load_max_1min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_1min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Checking negative limit for 1min average.";
 
-$a=SlurmHC::Load::run( load_max_5min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_5min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Checking negative limit for 5min average.";
 
-$a=SlurmHC::Load::run( load_max_15min=>$load ); #print Dumper($a);
+$a=SlurmHC::Load->run( load_max_15min=>$load ); #print Dumper($a);
 cmp_deeply $a, $res, "Checking negative limit for 15min average.";
 
 $res->{warning}=[];
